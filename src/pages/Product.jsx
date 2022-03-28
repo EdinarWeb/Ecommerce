c71e6cf778bdf2d9e2d2ca3aa33e8782fa22deaa
EdinarWeb <edinarweb@gmail.com>
2022-03-27 23:39:48 -0300

    Se añadieron secciones nuevas

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,13 +1,161 @@
+import { Add, Remove } from '@mui/icons-material';
+import styled from "styled-components";
+import Navbar from "../components/Navbar";
+import Announcement from "../components/Announcement";
+import Newsletter from "../components/Newsletter";
+import Footer from "../components/Footer";
 
 
 const Container = styled.div``;
 
+const Wrapper = styled.div`
+  padding: 50px;
+  display: flex;
+`;
+
+const ImgContainer = styled.div`
+  flex: 1;
+`;
+
+const Image = styled.img`
+  width: 100%;
+  height: 90vh;
+  object-fit: cover;
+`;
+
+const InfoContainer = styled.div`
+  flex: 1;
+  padding: 0px 50px;
+`;
+
+const Title = styled.h1`
+  font-weight: 200;
+`;
+
+const Description = styled.p`
+  margin: 20px 0px;
+`;
+
+const Price = styled.span`
+  font-weight: 100;
+  font-size: 40px;
+`;
+
+const FilterContainer = styled.div`
+  width: 50%;
+  margin: 30px 0px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Filter = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const FilterTitle = styled.span`
+  font-size: 20px;
+  font-weight: 200;
+`;
+
+const FilterColor = styled.div`
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  background-color: ${props => props.color};
+  margin: 0px 5px;
+  cursor: pointer;
+`;
+
+const FilterSize = styled.select`
+  margin-left: 10px;
+  padding: 5px;
+`;
+
+const FilterSizeOption = styled.option``;
+
+const AddContainer = styled.div`
+  width: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const AmountContainer = styled.div`
+  display: flex;
+  align-items: center;
+  font-weight: 700;
+`;
+
+const Amount = styled.span`
+  width: 30px;
+  height: 30px;
+  border-radius: 10px;
+  border: 1px solid teal;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 0px 5px;
+`;
+
+const Button = styled.button`
+  padding: 15px;
+  border: 2px solid teal;
+  background-color: white;
+  cursor: pointer;
+  font-weight: 500;
+  &:hover{
+      background-color: #f8f4f4;
+  }
+`;
+
 const Product = () => {
-  return (
-    <Container>
-        
-    </Container>
-  )
+    return (
+        <Container>
+            <Navbar />
+            <Announcement />
+            <Wrapper>
+                <ImgContainer>
+                    <Image src="https://i.ibb.co/S6qMxwr/jean.jpg" />
+                </ImgContainer>
+                <InfoContainer>
+                    <Title>Denim Jumsuit</Title>
+                    <Description>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ducimus maiores commodi explicabo tenetur deserunt veritatis fuga enim corporis vel libero natus porro suscipit, quam temporibus molestias at doloribus incidunt harum.
+                    </Description>
+                    <Price>$ 20</Price>
+                    <FilterContainer>
+                        <Filter>
+                            <FilterTitle>Color</FilterTitle>
+                            <FilterColor color="black" />
+                            <FilterColor color="darkblue" />
+                            <FilterColor color="gray" />
+                        </Filter>
+                        <Filter>
+
+                            <FilterTitle>Size</FilterTitle>
+                            <FilterSize>
+                                <FilterSizeOption>XS</FilterSizeOption>
+                                <FilterSizeOption>S</FilterSizeOption>
+                                <FilterSizeOption>M</FilterSizeOption>
+                                <FilterSizeOption>L</FilterSizeOption>
+                                <FilterSizeOption>XL</FilterSizeOption>
+                            </FilterSize>
+                        </Filter>
+                    </FilterContainer>
+                    <AddContainer>
+                        <AmountContainer>
+                            <Remove />
+                            <Amount>1</Amount>
+                            <Add />
+                        </AmountContainer>
+                        <Button>ADD TO CART</Button>
+                    </AddContainer>
+                </InfoContainer>
+            </Wrapper>
+            <Newsletter />
+            <Footer />
+        </Container>
+    )
 }
 
 export default Product
---
+++ src/pages/Register.jsx
@@ -0,0 +1,11 @@
+const Container = styled.div``;
+
+const Register = () => {
+  return (
+    <Container>
+        
+    </Container>
+  )
+}
+
+export default Register
